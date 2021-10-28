6aa3aeb6e9b29c7b152f48679b568e6a47114e44
Leekibeom <lkb951103@gmail.com>
2021-10-28 10:30:46 +0900

    Challenge_five

--- src/index.js
+++ src/index.js
@@ -1,28 +1,28 @@
-const title = document.querySelector("h1");
-const timer = document.querySelector(".js-clock");
+const colors = [
+  "#ef5777",
+  "#575fcf",
+  "#4bcffa",
+  "#34e7e4",
+  "#0be881",
+  "#f53b57",
+  "#3c40c6",
+  "#0fbcf9",
+  "#00d8d6",
+  "#05c46b",
+  "#ffc048",
+  "#ffdd59",
+  "#ff5e57",
+  "#d2dae2",
+  "#485460",
+  "#ffa801",
+  "#ffd32a",
+  "#ff3f34",
+];
+const btn = document.querySelector("button");
 
-function getTime() {
-  const nowDate = new Date();
-  const xMasDate = new Date("2021-12-24:00:00:00");
-  const dDay = xMasDate - nowDate;
-  const day = String(Math.floor(dDay / (1000 * 60 * 60 * 24))).padStart(2, "0");
-  const hour = String(
-    Math.floor((dDay % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-  ).padStart(2, "0");
-  const min = String(
-    Math.floor((dDay % (1000 * 60 * 60)) / (1000 * 60))
-  ).padStart(2, "0");
-  const sec = String(Math.floor((dDay % (1000 * 60)) / 1000)).padStart(2, "0");
-
-  if (dDay <= 0) {
-    title.innerText = `Merry Chrismas~!!!`;
-  } else {
-    timer.innerText = `${day}D ${hour}H ${min}M ${sec}S`;
-  }
+function colorChange() {
+  const colorOne = Math.floor(Math.random() * colors.length);
+  const colorTwo = Math.floor(Math.random() * colors.length);
+  document.body.style.background = `linear-gradient(to left, ${colors[colorOne]}, ${colors[colorTwo]})`;
 }
-
-function init() {
-  getTime();
-  setInterval(getTime, 1000);
-}
-init();
+btn.addEventListener("click", colorChange);
